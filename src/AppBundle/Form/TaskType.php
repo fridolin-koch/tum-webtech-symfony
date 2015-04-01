d05c154f77f19a0d646a90fbfb74106637247088
Frido Koch <info@fridokoch.de>
2015-04-01 19:14:48 +0200

    Added controller and views for Task entity

---
+++ src/AppBundle/Form/TaskType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TaskType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description', 'textarea')
+            ->add('dueDate')
+            ->add('timeEstimated')
+            ->add('priority')
+            ->add('type')
+            ->add('state')
+            ->add('project')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Task'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'appbundle_task';
+    }
+}
